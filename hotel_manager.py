5d061a16079abc43858d6fa3247a47b8001f2715
Sean R Reid <sean@torchcodelab.com>
2020-12-13 13:18:03 -0500

    Added checks to see if a room is occupied or not

--- hotel_manager.py
+++ hotel_manager.py
@@ -10,7 +10,6 @@ hotel = {
   }
 }
 
-# Let's move the check in and check out process into a function.
 def start_check_in_or_out():
     # Default value is None
     checkinout = None
@@ -24,49 +23,76 @@ def start_check_in_or_out():
             print('I\'m sorry, I only understand "in" or "out". Please reply with "in" or "out."')
             checkinout = None
 
-# Let's clean up the redundancy with the floor and room checks.
 def get_room_and_floor():
-    # We're going to resume the best and assume a user inputs only numbers
     floor = int(input('Which floor would you prefer?'))
     room = int(input('Which room would you prefer?'))
-    # Return the floor and then the room.
-    # This will allow us to access them in the 0 and 1 index order
     return floor, room
 
-def do_checkin(location, people):
-    # Does the chosen floor exist?
-    # We can print the keys in the hotel dictionary to check.
+# Let's verify the room's status.
+def is_room_empty(floor, room):
+    # Let's check the rooms
     print(hotel.keys())
+    # Is this floor in the hotel?
+    if floor in hotel.keys():
+        #If so, is the room on this floor?
+        if room in hotel[floor].keys() and len(hotel[floor][room]) > 0:
+            # We have a room, is it full?
+            return False
+        else:
+            return True
+    else:
+        # Floor doesn't exist, ergo the room is empty
+        return True
+    # Default value, assumes room exists and is occupied
+    return False
+
+def do_checkin(location):
+    occupant = input('What is your name?')
     if location[0] in hotel.keys():
-        # If it exists, check in the user
         hotel[location[0]][location[1]] = occupant
     else:
-        # If the floor doesn't exist we'll need to create it.
-        # This prevents KeyError problems.
         hotel[location[0]] = {}
         hotel[location[0]][location[1]] = occupant
+    # Assume we don't have any check in issues and return True.
+    # This will probably change.
+    return True
 
 def do_checkout(location):
-    # Let's make sure we're checking out from a room a floor that exists
-    # At some point, we'll want to check the room exists too.
-    if location[0] in hotel.keys():
-        del hotel[location[0]][location[1]]
-    else:
-        print("That floor/room is not in this hotel.")
-    # Right now, this will end the program,
-    # we'll want to give the user a chance to go back and try a different room/floor
-
+    # This can be simplified.
+    # We've handed of a lot of the room checks to the is_room_empty() function
+    del hotel[location[0]][location[1]]
+    return True
 
 status = start_check_in_or_out()
 
 if status == 'in':
-    # Run the new function to get the room and floor choices
-    location = get_room_and_floor()
-    occupant = input('What is your name?')
-    do_checkin(location, occupant)
+    # We're going to setup a loop if a user isn't able to check in to a particular room.
+    # If they can check in, they'll be done.
+    # If not, we'll prompt for a new floor/room choice.
+    checked_in = False
+    while checked_in == False:
+        location = get_room_and_floor()
+        room_empty = is_room_empty(location[0], location[1])
+        # Is the room empty? If so, check in!
+        if room_empty == True:
+            checked_in = do_checkin(location)
+        else:
+            # If not, let the user know and go back the start of this loop.
+            print("That room is occupied, please choose another room.")
+            checked_in = False
 
 elif status == 'out':
-    location = get_room_and_floor()
-    do_checkout(location)
+    checked_out = False
+    while checked_out == False:
+        location = get_room_and_floor()
+        # Are we checking out of an occupied room?
+        room_empty = is_room_empty(location[0], location[1])
+        if room_empty == True:
+            # If the room isn't occupied, go back to the start of the loop.
+            print("There isn't anyone in that room.")
+            checked_out = False
+        else:
+            # Otherwise, try to check the user out
+            checked_out = do_checkout(location)
 
 print(hotel)
